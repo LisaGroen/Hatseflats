06a2aece44947a92802fa46079d052e94b3d7f0c
LisaGroen <lgroen@roc-dev.com>
2016-10-26 11:16:10 +0200

    h11 opdracht 5 af

---
+++ src/h11/Opdracht5.java
@@ -0,0 +1,26 @@
+package h11;
+
+import java.awt.*;
+import java.applet.*;
+
+/**
+ * Created by Gebruiker on 26-10-2016.
+ * Af
+ */
+public class Opdracht5 extends Applet{
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        int teller = 0;
+        int y = 0;
+
+        while(teller < 5) {
+            y += 20;
+            g.drawRect(y, y, 20, 20);
+            teller++;
+        }
+
+
+    }
+}
