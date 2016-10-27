e18153bb33c2cac1b1f07a5e8698722591c36bad
LisaGroen <lgroen@roc-dev.com>
2016-10-27 08:45:34 +0200

    h11 opdracht 9 af

---
+++ src/h11/Opdracht9.java
@@ -0,0 +1,40 @@
+package h11;
+
+import java.awt.*;
+import java.applet.*;
+
+/**
+ * Created by Gebruiker on 26-10-2016.
+ * Af
+ */
+public class Opdracht9 extends Applet{
+
+    public void paint(Graphics g) {
+        int x = 50;
+        int y = 50;
+        for (int row = 0; row < 4; row++) {
+            x = 50;
+            y += 40;
+            for (int kolom = 0; kolom < 8; kolom++) {
+                if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                    g.setColor(Color.black);
+                    g.fillRect(x, y, 40, 40);
+                } else {
+                    g.setColor(Color.white);
+                    g.fillRect(x, y, 40, 40);
+                } x += 40;
+            }
+            x = 50;
+            y += 40;
+            for (int kolom = 0; kolom < 8; kolom++) {
+                if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                    g.setColor(Color.white);
+                    g.fillRect(x, y, 40, 40);
+                } else {
+                    g.setColor(Color.black);
+                    g.fillRect(x, y, 40, 40);
+                } x += 40;
+            }
+        }
+    }
+}
